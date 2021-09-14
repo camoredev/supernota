f307153d7a1ed396ca05c7de598f3237d0d7473a
Caio Moraes <47901065+camoredev@users.noreply.github.com>
2021-09-14 03:17:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var nome = "Brenndha";
+
+var subtitle = document.querySelector('.page-subtitle')
+
+
+function calcular(){
+  let primeiroBimestre = document.getElementById('nota1');
+  let segundoBimestre = document.getElementById('nota2');
+
+  let notaFinal = parseFloat(primeiroBimestre) + parseFloat(segundoBimestre)
+  console.log(notaFinal)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
